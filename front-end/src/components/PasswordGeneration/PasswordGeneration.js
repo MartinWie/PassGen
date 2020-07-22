36dcff0ebfc30f28f672a57e82d44d26ce8572c2
MartinWie <martin.wiechmann.office@gmail.com>
2020-07-22 09:52:53 +0200

    test

---
+++ front-end/src/components/PasswordGeneration/PasswordGeneration.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import './PasswordGeneration.css';
+import { FormControl, Button, InputGroup, Form } from 'react-bootstrap';
+//import useForm from 'react-hook-form';
+//pivot: learn to create better forms and improve css, than come back 
+
+//had to coement that out because css overwrites button css (border radius figure remove of botstrap or overwite )
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import { ArrowClockwise } from 'react-bootstrap-icons';
+const randomString = require('../../utils/getRandomString')
+
+function PasswordGeneration() {
+
+  const [password,setPassword] = useState("Choose a length!");
+  const [pwLength,setPwLength] = useState("length");
+  const [checkboxState, setCheckboxState] = useState({
+    checkboxNumbers: true,
+    checkboxSpecialChars: true,
+    checkboxUpper: true,
+    checkboxLower:true
+  });
+
+  return <div id="main">
+      <h3>Enter a length for your password</h3>
+
+      <div className="HorizontalLayout">
+        <FormControl className="Fullwidth"
+          placeholder={pwLength}
+          aria-label={pwLength}
+          aria-describedby="basic-addon2"
+          onChange={evt => handleChange(evt)}
+        />
+        <InputGroup.Append>
+          <Button onClick={handleClick} variant="outline-secondary"><ArrowClockwise className="ButtonImage" /></Button>
+        </InputGroup.Append>
+      </div>
+      <div className="HorizontalLayout" >
+        <Form.Check id="checkboxNumbers" onChange={handleChangeCheckbox} className="PWGenCheckbox" type="checkbox" label="Numbers" checked={checkboxState["checkboxNumbers"]} />
+        <Form.Check id="checkboxSpecialChars" onChange={handleChangeCheckbox} className="PWGenCheckbox" type="checkbox" label="Special characters" checked={checkboxState["checkboxSpecialChars"]} />
+        <Form.Check id="checkboxUpper" onChange={handleChangeCheckbox} className="PWGenCheckbox" type="checkbox" label="Upper case" checked={checkboxState["checkboxUpper"]} />
+        <Form.Check id="checkboxLower" onChange={handleChangeCheckbox} className="PWGenCheckbox" type="checkbox" label="Lower case" checked={checkboxState["checkboxLower"]} />
+      </div>
+      <div className="HorizontalLayout">
+        <Form.Control className="Fullwidth" as="textarea" rows="8" readOnly={true} value={password} />
+      </div>
+    </div>;
+
+
+  function handleChangeCheckbox(evt){
+    //Using he Object spread because setCheckboxState requires a new object for rerendering(Object spread helps us to create a new Object instead of copying the reference!)
+    let tmp_CheckboxState = {...checkboxState}
+    tmp_CheckboxState[evt.target.id] = !tmp_CheckboxState[evt.target.id]
+
+    setCheckboxState(tmp_CheckboxState)
+  }
+
+  function handleClick() {
+    setPassword(randomString(pwLength,checkboxState))
+  }
+
+  function handleChange(evt){
+    //saving tmp_pwLength in tmp const because setState is async
+    const tmp_pwLength = parseInt(evt.target.value, 10)
+    if(Number.isInteger(tmp_pwLength)){
+      setPwLength(tmp_pwLength)
+      setPassword(randomString(tmp_pwLength,checkboxState))
+    } else {
+      setPwLength("Choose a length!")
+      setPassword("Choose a length!")
+    }
+  }
+}
+
+export default PasswordGeneration;
