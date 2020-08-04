8e59fbf2544fd99ba08350f4af43505a85170ee5
MartinWie <martin.wiechmann.office@gmail.com>
2020-08-04 07:32:44 +0200

    added global theming for material-ui

--- front-end/src/components/Words/Words.js
+++ front-end/src/components/Words/Words.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { useForm } from "react-hook-form";
 import { Slider } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
-
-import { createMuiTheme } from '@material-ui/core/styles';
+import theme from '../../config/theme';
 
 const {register, handleSubmit, errors} = useForm;
 
@@ -11,16 +10,7 @@ const {register, handleSubmit, errors} = useForm;
 // + refine a good theme with currently picked colors: https://in-your-saas.github.io/material-ui-theme-editor/
 // Now: learn theming :)
 // Theming problem slider read all and understand! https://stackoverflow.com/questions/50831450/how-to-overwrite-classes-and-styles-in-material-ui-react
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#0088A9',
-    },
-    secondary: {
-      main: '#edf0f1',
-    }
-  },
-});
+
 
 
 function valuetext(value) {
