bf3c69774dec7755f4e527c053aa8fa8b0e11de4
MartinWie <martin.wiechmann.office@gmail.com>
2020-08-05 08:05:25 +0200

    added material-ui Icons improved Passwordgeneration.js played around with styling/material-ui components

--- front-end/src/components/Words/Words.js
+++ front-end/src/components/Words/Words.js
@@ -1,31 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useForm } from "react-hook-form";
 import { Slider,FormLabel } from '@material-ui/core';
-
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 const {register, handleSubmit, errors} = useForm;
-
-// if material ui is good create a constants folder and put the theme there
-// + refine a good theme with currently picked colors: https://in-your-saas.github.io/material-ui-theme-editor/
-// Now: learn theming :)
-// Theming problem slider read all and understand! https://stackoverflow.com/questions/50831450/how-to-overwrite-classes-and-styles-in-material-ui-react
-
 
 
 function valuetext(value) {
   return {value};
-
+  
 }
 function Words() {
-
+  const [checkboxState, setCheckboxState] = useState({
+    checkedGer: true,
+    checkedEng: true
+  });
+  
   return <div className="toolsframe">
-      <a>Words tbd</a>
-      <form>
-
-      </form>
       
-      <div className="">
-        Number of words:
+      <span>Number of words:</span>
+      <div className="">        
         <Slider
           defaultValue={4}
           getAriaValueText={valuetext}
@@ -36,9 +34,62 @@ function Words() {
           max={8}
           valueLabelDisplay="auto"
         />
-        <FormLabel>Test label(move right to slider and display sliders value)</FormLabel>
+        <FormLabel>4</FormLabel>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checkboxState.checkedGer}
+              onChange={handleChangeCheckbox}
+              name="checkedGer"
+              color="primary"
+            />
+          }
+          label="German"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checkboxState.checkedEng}
+              onChange={handleChangeCheckbox}
+              name="checkedEng"
+              color="primary"
+            />
+          }
+          label="English"
+        />
+        <TextField id="outlined-separator" label="Separator" variant="outlined" />
+        <Button variant="contained" color="primary"> <RefreshIcon /></Button>
+      </div>
+      <div>
+        <TextField
+          placeholder="Yeah! PassWords"
+          multiline
+          rows={3}
+          rowsMax={Infinity}
+          fullWidth
+        />
       </div>
   </div>
+
+function handleChangeCheckbox(evt){
+  //Using he Object spread because setCheckboxState requires a new object for rerendering(Object spread helps us to create a new Object instead of copying the reference!)
+  let tmp_CheckboxState = {...checkboxState}
+  tmp_CheckboxState[evt.target.id] = !tmp_CheckboxState[evt.target.id]
+
+  setCheckboxState(tmp_CheckboxState)
+}
+
+//found more elegant solution(replace here and in pw gen):
+
+/*
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+*/
+
+
+
+
 }
 
 export default Words;
