a12c4cf92d7584bb158f23f143c0b13782b1639d
MartinWie <martin.wiechmann.office@gmail.com>
2020-08-21 09:33:19 +0200

    added fomatting / layout

--- front-end/src/components/Keypair/Keypair.js
+++ front-end/src/components/Keypair/Keypair.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Button, TextField} from '@material-ui/core/';
 import RefreshIcon from '@material-ui/icons/Refresh';
+formattingimport VisibilityIcon from '@material-ui/icons/Visibility';
 import {makeStyles} from '@material-ui/core/styles'
 import theme from '../../config/theme';
 import './Keypair.css'
@@ -26,41 +27,51 @@ function Keypair(){
           onChange={(evt) => setNameState(evt.target.value)}
         />
         <Button variant="contained" color="primary" onClick={() => console.log(`Gen keypair! ${nameState}`) }> <RefreshIcon /></Button>
+        
+        <div className="key-container">
+          Public key:
+          <TextField
+            placeholder="Yeah! PassWords"
+            multiline
+            variant="outlined"
+            rows={2}
+            rowsMax={Infinity}
+            fullWidth
+            value=""
+            InputProps={{
+              readOnly: true,
+              classes: {
+                notchedOutline: classes.notchedOutline
+              }
+            }}
+          />
+        </div>
 
-        <TextField
-          placeholder="Yeah! PassWords"
-          multiline
-          variant="outlined"
-          rows={2}
-          rowsMax={Infinity}
-          fullWidth
-          value="something"
-          InputProps={{
-            readOnly: true,
-            classes: {
-              notchedOutline: classes.notchedOutline
-            }
-          }}
-        />
-        <Button variant="contained" color="primary" onClick={() => console.log(`Show public key! ${nameState}`) }> <RefreshIcon /></Button>
-
-        <TextField
-          placeholder="Yeah! PassWords"
-          multiline
-          variant="outlined"
-          rows={2}
-          rowsMax={Infinity}
-          fullWidth
-          value="something"
-          InputProps={{
-            readOnly: true,
-            classes: {
-              notchedOutline: classes.notchedOutline
-            }
-          }}
-        />
-        <Button variant="contained" color="primary" onClick={() => console.log(`Show private key! ${nameState}`) }> <RefreshIcon /></Button>
-
+        <div className="key-container">
+          Private key:
+          <div className="private-key--block">
+            <div className="private-key">
+              <TextField
+                placeholder="Yeah! PassWords"
+                multiline
+                variant="outlined"
+                rows={2}
+                rowsMax={Infinity}
+                fullWidth
+                value=""
+                InputProps={{
+                  readOnly: true,
+                  classes: {
+                    notchedOutline: classes.notchedOutline
+                  }
+                }}
+              />
+            </div>
+            <div className="private-key--button">
+              <Button variant="contained" color="primary" onClick={() => console.log(`Show private key! ${nameState}`) }> <VisibilityIcon /></Button>
+            </div>
+          </div>
+        </div>
 
     </div>
 }
