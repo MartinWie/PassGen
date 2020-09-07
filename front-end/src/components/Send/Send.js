93d428026e1f52cdf6aa883cc71bb1ffe3b7303f
MartinWie <martin.wiechmann.office@gmail.com>
2020-09-07 09:46:00 +0200

    playing around for Send tab

--- front-end/src/components/Send/Send.js
+++ front-end/src/components/Send/Send.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Send.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+      backgroundColor: "transparent",
+    },
+    palette: {
+        text: {
+          primary: "#FFFFFF"
+        }
+    },
+  });
 
 function Send() {
+    const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    
+    
     return <div className="toolsframe">
-        <a>Send tbd</a>
+        <Paper className={classes.root}>
+        <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+        >
+            <Tab label="Password" value="Test"/>
+            <Tab label="Keypair" />
+        </Tabs>
+        </Paper>
     </div>
 }
-//Implement this one :)
+
 export default Send;
