0fa292c49e0784c2ec55ff6bb5a826ed78d8be98
MartinWie <martin.wiechmann.office@gmail.com>
2020-09-10 09:29:40 +0200

    updated SendGenerator

--- front-end/src/hooks/SendGenerator.js
+++ front-end/src/hooks/SendGenerator.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import PassContainer from './PassContainer'
 
 function SendGenerator(props) {
     
     return(
-        props.type
+        <div>
+            {props.type}  
+            <PassContainer value="Link to share the credential" />
+            {props.value == "password" ? <PassContainer value="Public key" /> : null}
+        </div>
     )
 }
-
+ // fix logik to show hide public key field, then add input for name/pw
 export default SendGenerator
