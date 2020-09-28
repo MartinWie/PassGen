8ffee195f61322c7c0453711d24a05d1afef9a17
MartinWie <martin.wiechmann.office@gmail.com>
2020-09-28 09:22:54 +0200

    styled SliderContainer

--- front-end/src/hooks/SliderContainer.js
+++ front-end/src/hooks/SliderContainer.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import {Slider} from '@material-ui/core/';
 import InputContainer from './InputContainer'
+import './SliderContainer.css'
 
 function SliderContainer(props) {
 
-  return <div>
-    <Slider
-      defaultValue={props.defaultValue ? props.defaultValue : 0}
-      aria-labelledby="discrete-slider-small-steps"
-      step={1}
-      marks
-      min={props.min}
-      max={props.max}
-      valueLabelDisplay="auto"
-      onChange={(evt, value) => props.setSliderValue(value)}
-    />
+  return <div className="slidercontainer">
+    <div className="slidercontainer__slider">
+      <Slider
+        defaultValue={props.defaultValue ? props.defaultValue : 0}
+        aria-labelledby="discrete-slider-small-steps"
+        step={1}
+        marks
+        min={props.min}
+        max={props.max}
+        valueLabelDisplay="auto"
+        onChange={(evt, value) => props.setSliderValue(value)}
+      />
+    </div>
+    
     <InputContainer hideDownloadbutton outlinedName="Length" inputChange={props.setSliderValue} redoFunction={props.redoButtonFunction} inputValue={props.sliderValue} />
       
   </div>
