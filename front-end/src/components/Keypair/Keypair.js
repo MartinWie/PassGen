df34d6a96fa3ee35b39fbb68f826136faa3df91b
MartinWie <martin.wiechmann.office@gmail.com>
2020-09-29 07:06:03 +0200

    removed old code

--- front-end/src/components/Keypair/Keypair.js
+++ front-end/src/components/Keypair/Keypair.js
@@ -1,17 +1,10 @@
 import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles'
-import theme from '../../config/theme';
 import './Keypair.css'
 import PassContainer from '../../hooks/PassContainer'
 import InputContainer from '../../hooks/InputContainer'
 
-const useStyles = makeStyles({
-    notchedOutline: {borderColor: theme.palette.primary.main + " !important"},
-});
-
 function Keypair(){
 
-    const classes = useStyles();
     const [nameState,setNameState] = useState("key")
 
     return <div className="toolsframe">
