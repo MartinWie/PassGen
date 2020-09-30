bafa50e10daff828b590458eb081fac2b30b0ad8
MartinWie <martin.wiechmann.office@gmail.com>
2020-09-30 08:28:34 +0200

    added mobile view for slidercontainer

--- front-end/src/hooks/SliderContainer.js
+++ front-end/src/hooks/SliderContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Slider} from '@material-ui/core/';
+import {isMobile} from "react-device-detect";
 import InputContainer from './InputContainer'
 import './SliderContainer.css'
 
 function SliderContainer(props) {
 
-  return <div className="slidercontainer">
+  return <div style={wordsInputStylingPerDevice()}>
     <div className="slidercontainer__slider">
       <Slider
         defaultValue={props.defaultValue ? props.defaultValue : 0}
@@ -22,6 +23,20 @@ function SliderContainer(props) {
     <InputContainer hideDownloadbutton outlinedName="Length" inputChange={props.setSliderValue} redoFunction={props.redoButtonFunction} inputValue={props.sliderValue} />
       
   </div>
+
+  function wordsInputStylingPerDevice(){
+    var style = {
+      display: 'flex'
+    }
+    if(isMobile){
+      style = {
+        display: 'grid'
+      }
+    }
+    return style;
+  }
+
 }
 
+ // Add mobile UI check and do display: flex; for desktop and grid for mobile
 export default SliderContainer;
