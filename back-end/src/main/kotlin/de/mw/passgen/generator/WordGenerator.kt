50e3c61057f2a1d8c8b67673cfc8a233694569a8
MartinWie <martin.wiechmann.office@gmail.com>
2020-10-09 09:07:29 +0200

    played around with testing+ added minimal test + learning tdd

--- back-end/src/main/kotlin/de/mw/passgen/generator/WordGenerator.kt
+++ back-end/src/main/kotlin/de/mw/passgen/generator/WordGenerator.kt
@@ -1,2 +1,13 @@
 package de.mw.passgen.generator
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class WordGenerator {
+
+    @GetMapping("/word")
+    fun getRandomWord():String{
+        return "Word"
+    }
+}
--- back-end/src/test/kotlin/de/mw/passgen/generator/WordGeneratorTest.kt
+++ back-end/src/test/kotlin/de/mw/passgen/generator/WordGeneratorTest.kt
@@ -1,2 +1,14 @@
 package de.mw.passgen.generator
 
+import org.junit.Assert
+import org.junit.Test
+
+class WordGeneratorTest {
+
+    @Test
+    fun testWordgenertorReturnsString(){
+        val generator = WordGenerator()
+        val ranString = generator.getRandomWord()
+        Assert.assertEquals(String().javaClass.kotlin.simpleName,ranString.javaClass.kotlin.simpleName)
+    }
+}
