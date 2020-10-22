208d869a6a55abdccd3907d90b0f6511a79f2cda
MartinWie <martin.wiechmann.office@gmail.com>
2020-10-22 09:59:45 +0200

    tried to fix the "There was an unexpected error (type=Not Found, status=404)." error with from spring

--- back-end/src/main/kotlin/de/mw/passgen/PassgenApplication.kt
+++ back-end/src/main/kotlin/de/mw/passgen/PassgenApplication.kt
@@ -2,8 +2,10 @@ package de.mw.passgen
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("de.7mw.passgen")
 class PassgenApplication
 
  // This is the "SpringConfiguration" class
