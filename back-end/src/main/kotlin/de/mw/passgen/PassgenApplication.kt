b1fb51157c26413399d7f57ba09033250ec4bec0
MartinWie <martin.wiechmann.office@gmail.com>
2020-10-22 10:35:57 +0200

    Switched to GetMapping and changed Controller to RestController which fixed the 404 error

--- back-end/src/main/kotlin/de/mw/passgen/PassgenApplication.kt
+++ back-end/src/main/kotlin/de/mw/passgen/PassgenApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("de.7mw.passgen")
+@ComponentScan("de.mw.passgen")
 class PassgenApplication
 
  // This is the "SpringConfiguration" class
--- back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorHandler.kt
+++ back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorHandler.kt
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
-@Controller
+@RestController
 class WordGeneratorHandler(
         val wordGeneratorService: WordGeneratorService
 ) {
@@ -16,13 +17,13 @@ class WordGeneratorHandler(
     @Autowired
     lateinit var wordRepository: WordRepository
 
-    @RequestMapping("/word")
+    @GetMapping("/word")
     fun getRandomWord():String{
         //return wordGeneratorService.getRandomWord()
         return "Word"
     }
 
-    @RequestMapping("/setup")
+    @GetMapping("/setup")
     fun setup():String{
         wordRepository.save(Word("Wort1","german"))
         wordRepository.save(Word( "Wort2","german"))
