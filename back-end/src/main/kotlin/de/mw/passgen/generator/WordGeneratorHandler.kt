df33af4a2a650514d1c611510001faa89e85bdb0
MartinWie <martin.wiechmann.office@gmail.com>
2020-10-22 10:37:29 +0200

    removed old now unused imports

--- back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorHandler.kt
+++ back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorHandler.kt
@@ -3,11 +3,8 @@ package de.mw.passgen.generator
 import de.mw.passgen.model.Word
 import de.mw.passgen.repository.WordRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 class WordGeneratorHandler(
