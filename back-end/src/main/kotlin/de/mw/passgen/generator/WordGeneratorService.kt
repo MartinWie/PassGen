4414ebda23c57cb20321308ca772b41c1630233e
MartinWie <martin.wiechmann.office@gmail.com>
2020-10-24 01:35:45 +0200

    added wordlist initialisation/loading

--- back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorService.kt
+++ back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorService.kt
@@ -4,10 +4,10 @@ import de.mw.passgen.model.Word
 import mu.KLogging
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
-import java.io.FileReader
 import java.io.IOException
 import de.mw.passgen.repository.WordRepository
 import org.springframework.beans.factory.annotation.Autowired
+import java.io.File
 
 
 @Service
@@ -23,11 +23,29 @@ class WordGeneratorService {
     }
 
     fun setup() : String{
+        val languages = mapOf<String,String>(
+                "german" to "src/main/kotlin/de/mw/passgen/generator/resources/deutsch.txt",
+                "english" to "src/main/kotlin/de/mw/passgen/generator/resources/english.txt"
+        )
+
         val result = wordRepository.findByValue("Wort")
 
         if(result.isEmpty()){
             logger.info("Initial setup")
-            wordRepository.save(Word("german","Wort"))
+
+            languages.map { language ->
+                val lineList = mutableListOf<Word>()
+
+                bufferedReaderFromFile(language.value)?.useLines { lines -> lines.forEach { line -> lineList.add(Word(language.key,line)) } }
+
+                logger.info { "Starting reading from BufferedReader" }
+
+                logger.info { "Words in list: ${lineList.size}" }
+
+                wordRepository.saveAll(lineList)
+
+            }
+
             return "done"
         }
 
@@ -35,11 +53,11 @@ class WordGeneratorService {
 
     }
 
-    private fun loadLanguageFromFile(lang: wordlistLanguages): BufferedReader? {
+    private fun bufferedReaderFromFile(lang: String): BufferedReader? {
         val br: BufferedReader?
 
         br = try{
-            BufferedReader(FileReader(lang.toString()))
+            File(lang).bufferedReader()
         }catch (e:IOException){
             logger.error(e.toString())
             null
@@ -48,11 +66,7 @@ class WordGeneratorService {
         return br
     }
 
-    object wordlistLanguages {
-        const val GERMAN = "..\\deutsch.txt"
-        const val ENGLISH = "..\\english.txt"
-    }
-
 }
 
+ // clenup saving / loading into different methodes
  // disable h2 console!
