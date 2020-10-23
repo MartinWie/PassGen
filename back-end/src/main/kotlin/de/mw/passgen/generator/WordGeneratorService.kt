d5ebf2ddc089564a0762fb5506c0a7dc8c3c8240
MartinWie <martin.wiechmann.office@gmail.com>
2020-10-23 23:34:24 +0200

    added setup logic

--- back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorService.kt
+++ back-end/src/main/kotlin/de/mw/passgen/generator/WordGeneratorService.kt
@@ -1,18 +1,38 @@
 package de.mw.passgen.generator
 
+import de.mw.passgen.model.Word
 import mu.KLogging
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
+import de.mw.passgen.repository.WordRepository
+import org.springframework.beans.factory.annotation.Autowired
+
 
 @Service
 class WordGeneratorService {
 
     companion object : KLogging()
 
+    @Autowired
+    lateinit var wordRepository: WordRepository
+
     fun getRandomWord():String{
         return "random"
+    }
+
+    fun setup() : String{
+        val result = wordRepository.findByValue("Wort")
+
+        if(result.isEmpty()){
+            logger.info("Initial setup")
+            wordRepository.save(Word("german","Wort"))
+            return "done"
+        }
+
+        return "nothing to do"
+
     }
 
     private fun loadLanguageFromFile(lang: wordlistLanguages): BufferedReader? {
