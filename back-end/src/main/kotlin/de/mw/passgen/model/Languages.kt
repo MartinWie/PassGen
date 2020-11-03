41d08ff33683eee64ad5e71e2edb0013550c7bc9
MartinWie <martin.wiechmann.office@gmail.com>
2020-11-03 08:59:09 +0100

    change imports and play around with spring context problem

--- back-end/src/main/kotlin/de/mw/passgen/model/Languages.kt
+++ back-end/src/main/kotlin/de/mw/passgen/model/Languages.kt
@@ -2,10 +2,7 @@ package de.mw.passgen.model
 
 import java.io.Serializable
 import java.util.*
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "Languages")
