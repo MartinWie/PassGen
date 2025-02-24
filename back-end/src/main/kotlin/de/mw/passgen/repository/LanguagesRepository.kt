8ed0992f79271f57bcc198b311d6926374938899
MartinWie <donotsuspend@googlegroups.com>
2025-02-24 19:01:14 +0100

    cleanup

--- back-end/src/main/kotlin/de/mw/passgen/repository/LanguagesRepository.kt
+++
@@ -1,8 +0,0 @@
-package de.mw.passgen.repository
-
-import de.mw.passgen.model.Languages
-import org.springframework.data.repository.CrudRepository
-
-interface LanguagesRepository : CrudRepository<Languages, Long> {
-    fun findByValue(value: String): List<Languages>
-}
